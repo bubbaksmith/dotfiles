30bdfdbb7e639887d3cddf7b7063c7b125c1b8cb
Brian Smith <brian.smith.keith@gmail.com>
2019-07-10 00:51:33 -0400

    tmux scripts and sync.sh

---
+++ sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yes | cp -rf ~/.bash_profile ~/src/dotfiles/.bash_profile
+yes | cp -rf ~/.bashrc ~/src/dotfiles/.bashrc
+yes | cp -rf ~/.gitconfig ~/src/dotfiles/.gitconfig
+yes | cp -rf ~/.k8s_hydra2_commands ~/src/dotfiles/.k8s_hydra2_commands
+yes | cp -rf ~/.vimrc ~/src/dotfiles/.vimrc
+
+
+yes | cp -rf ~/.tmux_scripts/battery ~/src/dotfiles/.tmux_scripts/battery
+yes | cp -rf ~/.tmux_scripts/git-branch ~/src/dotfiles/.tmux_scripts/git-branch
+yes | cp -rf ~/.tmux_scripts/kube-context ~/src/dotfiles/.tmux_scripts/kube-context
+
