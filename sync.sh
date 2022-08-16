42503d9a79d12d286c81af1f83e4743672218f7c
Brian Smith <brian.smith.keith@gmail.com>
2022-08-16 11:17:12 -0400

    add slack theme, update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-yes | cp -rf ~/.gitignore ~/src/dotfiles/.gitignore
+homedir_files_to_sync=(
+  .gitignore
+  .gitconfig
+  .git-commit-template
+  .bash_profile
+  .salesloft
+  .vimrc
+  .tmux.conf
+  .slack-theme
+)
 
-yes | cp -rf ~/.bash_profile ~/src/dotfiles/.bash_profile
-yes | cp -rf ~/.salesloft ~/src/dotfiles/.salesloft
-yes | cp -rf ~/.gitconfig ~/src/dotfiles/.gitconfig
-yes | cp -rf ~/.git-commit-template ~/src/dotfiles/.git-commit-template
+for file in ${homedir_files_to_sync[@]}; do
+  yes | cp -rf ~/${file} ~/src/dotfiles/${file}
+done
 
-yes | cp -rf ~/.vimrc ~/src/dotfiles/.vimrc
-#yes | cp -rf ~/.ideavimrc ~/src/dotfiles/.ideavimrc
-
-yes | cp -rf ~/.tmux.conf ~/src/dotfiles/.tmux.conf
 yes | cp -rf ~/.tmux_scripts/. ~/src/dotfiles/.tmux_scripts
-
 yes | cp -rf ~/.config/nvim/init.vim ~/src/dotfiles/.config/nvim/init.vim
 
 sed -i -E 's/signingkey = [A-Z0-9]*/signingkey = SUPERSEKRETKEY123456/g' .gitconfig
